8cdda0e0f58848300c4ddbf8536b98d8310f6765
JongChan Choi <jong@chan.moe>
2026-07-01 14:17:23 +0900

    Add bundle visualizer report

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import UnoCSS from "unocss/vite";
 
 export default defineConfig({
-  plugins: [UnoCSS(), react()],
+  plugins: [
+    UnoCSS(),
+    react(),
+    visualizer({
+      filename: "dist/bundle-stats.html",
+      gzipSize: true,
+      brotliSize: true,
+      template: "flamegraph",
+    }),
+  ],
   server: {
     port: 5173,
   },
